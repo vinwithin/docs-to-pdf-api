471e938c7ede71416d01e1536e98f41b4f13401f
vinwithin <kelvinadinata82@gmail.com>
2023-12-03 21:39:27 +0700

    env file

--- src/handler.js
+++ src/handler.js
@@ -1,14 +1,21 @@
 var docxConverter = require('docx-pdf');
+const {Storage} = require('@google-cloud/storage');
+const storage = new Storage();
+require('dotenv').config();
 
+const file_name  = 'output.pdf';
+const bucketName = process.env.BUCKET_NAME;
 async function uploadFiles(req, res) {
     const file = req.file.filename;
     const input = `./${file}`;
-    docxConverter('uploads/'+ input, 'uploads/output.pdf', function(err,result){
-        if(err){
-          console.log(err);
-        }
-        console.log('result'+result);
-      });
+    docxConverter('uploads/'+ input, `uploads/${file_name}`)
+    const options = {
+      destination: output,
+      
+    };
+  
+    await storage.bucket(bucketName).upload(`uploads/${file_name}`, options);
+    console.log(`${file_name} uploaded to ${bucketName}`);
       
     // console.log(input);
     res.json({ message: "Successfully uploaded files" });
