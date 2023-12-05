b9ca85ce19ccb8fdbb0857223eaee269507ace04
vinwithin <kelvinadinata82@gmail.com>
2023-12-05 18:38:56 +0700

    bug fixing phase 1

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 
 const multer = require("multer");
-const uploadFiles = require('./src');
+const uploadFiles = require('./src/handler');
     
 
 
--- src/handler.js
+++ src/handler.js
@@ -1,7 +1,10 @@
 var docxConverter = require('docx-pdf');
-const {Storage} = require('@google-cloud/storage');
-const storage = new Storage();
 require('dotenv').config();
+const {Storage} = require('@google-cloud/storage');
+const projectID = process.env.PROJECT_ID;
+const keyFileName = process.env.KEYFILENAME;
+const storage = new Storage({projectID, keyFileName});
+
 
 const file_name  = 'output.pdf';
 const bucketName = process.env.BUCKET_NAME;
@@ -10,17 +13,21 @@ async function uploadFiles(req, res) {
     const input = `./${file}`;
     docxConverter('uploads/'+ input, `uploads/${file_name}`)
     const options = {
-      destination: output,
+      destination: file_name,
       
     };
-  
+    try{
     await storage.bucket(bucketName).upload(`uploads/${file_name}`, options);
-    console.log(`${file_name} uploaded to ${bucketName}`);
     fs.rmSync(`uploads/${file_name}`, {
       force: true,
     });
+  
       
     // console.log(input);
-    res.json({ message: "Successfully uploaded files" });
+    
+  } catch(error){
+      console.log('Error', error)
+  }
+  res.json({ message: "Successfully uploaded files" });
 }
 module.exports = uploadFiles;
