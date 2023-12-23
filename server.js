eee0dd0f95d88f3ad0a207678d68b5bf5f253cc2
vinwithin <kelvinadinata82@gmail.com>
2023-12-23 11:20:31 +0700

    bug fix 2

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express')
-
+var docxConverter = require('docx-pdf');
+require('dotenv').config();
+const {Storage} = require('@google-cloud/storage');
 const multer = require("multer");
-
-const router = require("./src/handler")
+const fs = require("fs");
+const projectID = process.env.PROJECT_ID;
+const keyFileName = process.env.KEYFILENAME;
+const storageBucket = new Storage({projectID, keyFileName});
+const bucketName = process.env.BUCKET_NAME;
+const file_name  = 'output.pdf';
 
 
 
@@ -25,7 +31,26 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.use("/upload_files", router)
+app.post("/upload_files", upload.single("file"), function(req, res){
+  const file = req.file.filename;
+  
+  docxConverter(`uploads/${file}`, `uploads/${file_name}`, function(err, result){
+    if(err){
+      console.log("convert fail");
+    }
+      console.log("success convert");
+  });
+  const options = {
+    destination: file_name,
+    
+  };
+  storageBucket.bucket(bucketName).upload(`uploads/${file_name}`, options);
+  fs.rmSync(`uploads/${file_name}`, {
+    force: true,
+  });
+
+res.json({ message: "Successfully uploaded files" });
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
